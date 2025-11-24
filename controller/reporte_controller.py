9609699d1e3042c542f3ec24b89c659d2e6d4120
Franegeaa02 <egeafran02@gmail.com>
2025-11-24 20:32:57 -0300

    Agrega reportes

--- controller/reporte_controller.py
+++ controller/reporte_controller.py
@@ -24,34 +24,11 @@ class ReporteController:
         if not id_medico:
             raise ValueError("Debe seleccionar un médico.")
         return self.turno_service.obtener_turnos_por_medico_y_rango(id_medico, f_inicio, f_fin)
-import csv
-from datetime import datetime
-
-from services.medico_service import MedicoService
-from services.turno_service import TurnoService
-from services.paciente_service import PacienteService
-
-class ReporteController:
-    def __init__(self):
-        self.medico_service = MedicoService()
-        self.turno_service = TurnoService()
-        self.paciente_service = PacienteService()
-
-    def listar_medicos(self):
-        return self.medico_service.obtener_todos()
-
-    def obtener_reporte_turnos_medico(self, id_medico, f_inicio, f_fin):
-        self._validar_fechas(f_inicio, f_fin)
-        if not id_medico:
-            raise ValueError("Debe seleccionar un médico.")
-        
-        return self.turno_service.obtener_turnos_por_medico_y_rango(id_medico, f_inicio, f_fin)
 
     def obtener_reporte_pacientes(self, f_inicio, f_fin):
         self._validar_fechas(f_inicio, f_fin)
         return self.paciente_service.obtener_pacientes_atendidos_en_rango(f_inicio, f_fin)
 
-<<<<<<< HEAD
     def obtener_reporte_especialidad(self, f_inicio=None, f_fin=None):
         if f_inicio or f_fin:
             self._validar_fechas(f_inicio, f_fin)
@@ -62,11 +39,9 @@ class ReporteController:
             self._validar_fechas(f_inicio, f_fin)
         return self.turno_service.obtener_cantidad_turnos_por_estado(f_inicio, f_fin)
 
-=======
     # --------------------------------------------------------------------------
     # EXPORTACIÓN A CSV
     # --------------------------------------------------------------------------
->>>>>>> c40db1586329ccc3bb1c3f514a31c783000fb279
     def exportar_a_csv(self, datos, columnas, ruta_archivo):
         if not datos:
             raise ValueError("No hay datos para exportar.")
@@ -122,12 +97,8 @@ class ReporteController:
 
     def _validar_fechas(self, f_inicio, f_fin):
         if not f_inicio or not f_fin:
-<<<<<<< HEAD
             raise ValueError("Las fechas de inicio y fin son obligatorias si se filtra por fecha.")
         
-=======
-            raise ValueError("Las fechas de inicio y fin son obligatorias.")
->>>>>>> c40db1586329ccc3bb1c3f514a31c783000fb279
         try:
             d_inicio = datetime.strptime(f_inicio, "%Y-%m-%d")
             d_fin = datetime.strptime(f_fin, "%Y-%m-%d")
