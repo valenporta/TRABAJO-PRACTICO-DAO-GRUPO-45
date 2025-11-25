ec5223409ce282eea331e42fbffcdeaf5f34ff60
Franegeaa02 <egeafran02@gmail.com>
2025-11-24 22:54:25 -0300

    Agrega campo diagnostico en la reseta

--- model/receta.py
+++ model/receta.py
@@ -1,10 +1,11 @@
 
 class Receta:
-    def __init__(self, id_atencion, fecha, detalle, id_receta=None):
+    def __init__(self, id_atencion, fecha, detalle, id_receta=None, diagnostico=None):
         self.id_receta = id_receta
         self.id_atencion = id_atencion
         self.fecha = fecha
         self.detalle = detalle
+        self.diagnostico = diagnostico
 
     def __str__(self):
         return f"Receta {self.fecha}: {self.detalle[:20]}..."
